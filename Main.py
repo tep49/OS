8f63d8f644cb205480f5bebe556b93d9d6f131f6
tep49 <54687378+tep49@users.noreply.github.com>
2020-02-25 20:28:02 -0600

    Added a few outputs
    All the output and game logic should be solid, just needs synchronization and a way to exit the winning thread and continue to a new round

--- Main.py
+++ Main.py
@@ -10,9 +10,9 @@ def dealer(cards, log, i):
 
     for j in range(3):
         out = ("Player " + str(currentPlayer) + " gets " + str(cards[j][0]) + " of " + str(cards[j][1]) + " from the dealer\n")
-        currentPlayer += 1;
+        currentPlayer += 1
         if currentPlayer > 3:
-            currentPlayer = 1;
+            currentPlayer = 1
         log.write(out)
 
 
@@ -23,7 +23,10 @@ def player1(cards, log, roundNum,hand):
     else:
         hand.append(cards.pop(0))
 
-        log.write("Player 1 Hand: ")
+        log.write("Player 1 draws: ")
+        log.write(str(hand[1]))
+
+        log.write("\nPlayer 1 Hand: ")
         for x in hand:
             out = str(x) + ' '
             log.write(out)
@@ -32,11 +35,15 @@ def player1(cards, log, roundNum,hand):
         x = hand[0]
         y = hand[1]
         if (x[0] == y[0]):
-            print "win!"
+            log.write("\n\nPlayer 1 wins and exits\n\n")
+        #           Needs to exit and notify other threads, go to next round   
         else:
             z = random.randrange(0,1)
             cards.append(hand[z])
             hand.pop(z)
+            log.write("\nPlayer 1 discards: ")
+            log.write(str(hand[z]))
+
 
 
 def player2(cards, log, roundNum, hand):
@@ -46,7 +53,10 @@ def player2(cards, log, roundNum, hand):
     else:
         hand.append(cards.pop(0))
 
-        log.write("Player 2 Hand: ")
+        log.write("Player 2 draws: ")
+        log.write(str(hand[1]))
+
+        log.write("\nPlayer 2 Hand: ")
         for x in hand:
             out = str(x) + ' '
             log.write(out)
@@ -55,11 +65,15 @@ def player2(cards, log, roundNum, hand):
         x = hand[0]
         y = hand[1]
         if (x[0] == y[0]):
-            print "win!"
+            log.write("\n\nPlayer 2 wins and exits\n\n")
+        #           Needs to exit and notify other threads, go to next round
         else:
             z = random.randrange(0,1)
             cards.append(hand[z])
             hand.pop(z)
+
+            log.write("\nPlayer 2 discards: ")
+            log.write(str(hand[z]))
 
 
 def player3(cards, log, roundNum, hand):
@@ -69,7 +83,10 @@ def player3(cards, log, roundNum, hand):
     else:
         hand.append(cards.pop(0))
 
-        log.write("Player 3 Hand: ")
+        log.write("Player 3 draws: ")
+        log.write(str(hand[1]))
+
+        log.write("\nPlayer 3 Hand: ")
         for x in hand:
             out = str(x) + ' '
             log.write(out)
@@ -78,11 +95,15 @@ def player3(cards, log, roundNum, hand):
         x = hand[0]
         y = hand[1]
         if (x[0] == y[0]):
-            print "win!"
+            log.write("\n\nPlayer 3 wins and exits\n\n")
+#           Needs to exit and notify other threads, go to next round
         else:
             z = random.randrange(0,1)
             cards.append(hand[z])
             hand.pop(z)
+
+            log.write("\nPlayer 3 discards: ")
+            log.write(str(hand[z]))
 
 
 if __name__ == '__main__':
@@ -98,31 +119,37 @@ if __name__ == '__main__':
     p3Hand = []
 
     for i in range(3):
-        dealerThread = threading.Thread(group=None, target=dealer, name='dealer', args=(cards,log,i,), kwargs={})
+        dealerThread = threading.Thread(group=None, target=dealer, name='dealer', args=(cards,log,i,))
         dealerThread.start()
         roundNum = 0
+#       Currently have it set to loop a few times just to check output, will change to while win == 0 to loop until a thread wins
         while win >= 0:
 #           Should probably make this part better VVVVVVVV
             if i == 0:
-                p1Thread = threading.Thread(group=None, target=player1, name='player1', args=(cards,log,roundNum,p1Hand,), kwargs={})
+                p1Thread = threading.Thread(group=None, target=player1, name='player1', args=(cards,log,roundNum,p1Hand,))
                 p1Thread.start()
-                p2Thread = threading.Thread(group=None, target=player2, name='player2', args=(cards,log,roundNum,p2Hand,), kwargs={})
+                p2Thread = threading.Thread(group=None, target=player2, name='player2', args=(cards,log,roundNum,p2Hand,))
                 p2Thread.start()
-                p3Thread = threading.Thread(group=None, target=player3, name='player3', args=(cards,log,roundNum,p3Hand,), kwargs={})
+                p3Thread = threading.Thread(group=None, target=player3, name='player3', args=(cards,log,roundNum,p3Hand,))
                 p3Thread.start()
             if i == 1:
-                p2Thread = threading.Thread(group=None, target=None, name='player2', args=(), kwargs={})
+                p2Thread = threading.Thread(group=None, target=player2, name='player2', args=(cards,log,roundNum,p2Hand,))
                 p2Thread.start()
-                p3Thread = threading.Thread(group=None, target=None, name='player3', args=(), kwargs={})
+                p3Thread = threading.Thread(group=None, target=player3, name='player3', args=(cards,log,roundNum,p3Hand,))
                 p3Thread.start()
-                p1Thread = threading.Thread(group=None, target=player1, name='player1', args=(cards,log,i,), kwargs={})
+                p1Thread = threading.Thread(group=None, target=player1, name='player1', args=(cards,log,roundNum,p1Hand,))
                 p1Thread.start()
             if i == 2:
-                p3Thread = threading.Thread(group=None, target=None, name='player3', args=(), kwargs={})
+                p3Thread = threading.Thread(group=None, target=player3, name='player3', args=(cards,log,roundNum,p3Hand,))
                 p3Thread.start()
-                p1Thread = threading.Thread(group=None, target=player1, name='player1', args=(cards,log,i,), kwargs={})
+                p1Thread = threading.Thread(group=None, target=player1, name='player1', args=(cards,log,roundNum,p1Hand,))
                 p1Thread.start()
-                p2Thread = threading.Thread(group=None, target=None, name='player2', args=(), kwargs={})
+                p2Thread = threading.Thread(group=None, target=player2, name='player2', args=(cards,log,roundNum,p2Hand,))
                 p2Thread.start()
             roundNum += 1
+
+            log.write("\n\nCURRENT DECK: \n")
+            for z in cards:
+                log.write(str(z))
+            log.write("\n\n")
             win -= 1
